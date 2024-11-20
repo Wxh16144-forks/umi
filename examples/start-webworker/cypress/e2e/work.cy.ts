7429a60be5ad199dc86699309ccb4be8c3791250
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2024-11-20 18:04:18 +0800

    test: add e2e test

---
+++ examples/start-webworker/cypress/e2e/work.cy.ts
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('Basic Test', () => {
+  it('should work', () => {
+    cy.visit('/docs');
+    cy.get('input').should('have.value', '@Wxh16144');
+
+    cy.get('button').click();
+
+    cy.wait(500);
+    cy.contains('@WXH16144');
+  });
+});
