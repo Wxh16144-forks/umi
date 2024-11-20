5c77d3dd111f4c4084719394608349e6e0f3fced
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2024-11-20 18:46:36 +0800

    chore: fix path

--- examples/start-webworker/cypress/e2e/work.cy.ts
+++ examples/start-webworker/cypress/e2e/work.cy.ts
@@ -2,7 +2,7 @@
 
 describe('Basic Test', () => {
   it('should work', () => {
-    cy.visit('/docs');
+    cy.visit('/app/docs');
     cy.get('input').should('have.value', '@Wxh16144');
 
     cy.get('button').click();
